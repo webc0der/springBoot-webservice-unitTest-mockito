d251bc2b63601ce3d76c06616d82322be1dc1fda
manish <manish.singh@travelspends.com>
2020-02-15 23:25:05 +0530

    initial commit

---
+++ src/main/java/com/pratice/springboot/webservicemockitoTests/WebserviceMockitoTestsApplication.java
@@ -0,0 +1,13 @@
+package com.pratice.springboot.webservicemockitoTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceMockitoTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserviceMockitoTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratice/springboot/webservicemockitoTests/WebserviceMockitoTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratice.springboot.webservicemockitoTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceMockitoTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
